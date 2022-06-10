5d81978955241897303eec725747405804c8f82d
José Vitor Ramos Moraes <84106836+jvramos-py@users.noreply.github.com>
2022-06-09 21:14:55 -0300

    Source Code of game.

---
+++ imagens.js
@@ -0,0 +1,26 @@
+// imagens e sons do jogo
+
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//sons do jogo
+
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
+function preload(){
+  imagemDaEstrada = loadImage("estrada.jpg");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");  
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro]
+  
+  somDaTrilha = loadSound("sons/trilha.mp3");
+  somDaColisao = loadSound("sons/colidiu.m4a");
+  somDoPonto = loadSound("sons/ponto.mp3");
+}
---
+++ sons/carro.js
@@ -0,0 +1,36 @@
+//código do carro
+
+let xCarros = [600, 600, 600, 600];
+let yCarros = [40, 140, 210, 295];
+let velocidadeCarros = [3, 3.5, 4.2, 6];
+let comprimentoCarro = 70;
+let alturaCarro = 60;
+
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+}
+
+function movimentaCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    if (passouTodaATela(xCarros[i])){
+      xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarro){
+  return xCarro < - 50;
+}
+
+
+
+
+
